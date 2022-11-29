074a9469c62eca5abce299d0e42265f0f485eca8
Alex <fernandesalexandre1996@gmail.com>
2022-11-29 16:01:01 +0100

    create service to handle firebase requests

---
+++ service/notation.ts
@@ -0,0 +1,29 @@
+import { collection, doc, DocumentData, getDocs, limit, orderBy, query, QueryDocumentSnapshot, setDoc, where } from 'firebase/firestore';
+import { firestore } from '../firebase';
+import { average } from '../utils';
+
+const notationCollection = collection(firestore,'notation');
+
+const getNotation = async (restaurantId: string) => {
+  const restaurantRef = doc(firestore,`restaurants/${restaurantId}`)
+  const notationQuery = query(notationCollection, where('restaurant', "==", restaurantRef));
+  const queryNotationSnapshot = await getDocs(notationQuery);
+  const result: number[] = [];
+  queryNotationSnapshot.docs.forEach((notation) => {
+    result.push(notation.get('notation'))
+  })
+  return average(result);
+}
+
+const addNotation = async (restaurantId:string, newValue: number) => {
+  const timestamp: string = Date.now().toString();
+  const restaurantRef = doc(firestore,`restaurants/${restaurantId}`)
+  const _notation = doc(firestore,`notation/${timestamp}`);
+  await setDoc(_notation,{
+    "notation": newValue,
+    "restaurant": restaurantRef
+  });
+  return await getNotation(restaurantId)
+}
+
+export {getNotation, addNotation}
