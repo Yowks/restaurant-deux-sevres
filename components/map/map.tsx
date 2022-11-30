b7a83394100114fbac4b10de5e151ed945cbecbc
Alex <fernandesalexandre1996@gmail.com>
2022-11-30 09:49:32 +0100

    feat: retieve coordinates from address and set pointer

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,21 +1,29 @@
+import {useEffect} from 'react';
 import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import "leaflet-defaulticon-compatibility";
-import { RestaurantInterface } from '../../interface/restaurant';
+import { getCoordsFromAddress } from '../../utils';
 
-const Map: React.FC<RestaurantInterface> = ({address, name}) => {
-  const lat=0; const lon = 0
-  
+interface Props {
+  address?: string,
+  name?: string
+}
+
+const Map: React.FC<Props> = ({address, name}) => {
+  const lat=0; 
+  const lon=0;
+
   return (
     <MapContainer center={[ 46.5926541, -0.3962844]} zoom={13} scrollWheelZoom={false} style={{height:"100%"}}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[ lat, lon]}>
+    <Marker position={[ 46.5926541, -0.3962844]}>
       <Popup>
-        {name}
+        {/* {name} */}
+        Les Deux-Sèvres
       </Popup>
     </Marker>
   </MapContainer>
